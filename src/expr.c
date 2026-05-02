978b1e756decc625838e0068cd0e79054574ce7b
cjRem44x <remillardc15@gmail.com>
2026-05-02 16:00:14 -0400

    fill in funcs

--- src/expr.c
+++ src/expr.c
@@ -8,12 +8,50 @@ ExprList * init_ExprList(size_t cap)
     p->size = 0;
     p->cap = cap;
     p->dat = (Expr *)malloc(p->cap * sizeof(Expr));
+    
+    p->resize=resize;
+    p->add=add;
+    p->get=get;
+    p->rmv=rmv;
+    p->_free=_free;
     return p;
 }
 
-void resize(struct ExprList * slf);
-void add(struct ExprList * slf, Expr e);
-Expr * get(struct ExprList * slf, size_t idx);
-void rmv(struct ExprList * slf, size_t idx);
-void _free(struct ExprList * slf);
+void resize(struct ExprList * slf)
+{
+    if (slf->size >= slf->cap) {
+        slf->cap *= 2;
+        slf->dat = realloc(slf->dat, slf->cap * sizeof(Expr));
+    }
+}
 
+void add(struct ExprList * slf, Expr e)
+{
+    resize(slf);
+    slf->dat[slf->dat++] = e;
+}
+
+Expr * get(struct ExprList * slf, size_t idx)
+{
+    if (idx >= slf->size) return NULL;
+
+    return &slf->dat[idx];
+}
+
+void rmv(struct ExprList * slf, size_t idx)
+{
+    if (idx >= slf->size) return;
+
+    for (size_t i=idx; i<slf->size-1; i++)
+    {
+        slf->dat[i] = slf->dat[i+1];
+    }
+
+    slf->size--;
+}
+
+void _free(struct ExprList * slf)
+{
+    //for (size_t i=0; i<slf->size; i++){}
+    free(slf->dat);
+}
